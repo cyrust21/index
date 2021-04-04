88f385081a49091fa87f798bb39b388000b2b709
cyrust21 <45724743+cyrust21@users.noreply.github.com>
2021-04-05 00:35:45 +0700

    Update time.php

--- time.php
+++ time.php
@@ -1,6 +1,3 @@
 <?php
-echo "Today is " . date("Y/m/d") . "<br>";
-echo "Today is " . date("Y.m.d") . "<br>";
-echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
+echo "The time is " . date("h:i:sa");
 ?>
